6dc7df99f94eb9fb34fa6a87c7b953b500e8736f
s3anfinnegan <sean2001finnegan>
2023-06-08 13:38:24 +0100

    Expiry dates added

--- dashboard.js
+++ dashboard.js
@@ -1,45 +1,77 @@
-// Sample data for tomatoes and lettuce inventory levels, sales, and expiry dates
-const tomatoesInventoryData = [
-  { week: "Jan 1-7", level: 100, expiry: "2023-06-30" },
-  { week: "Jan 8-14", level: 80, expiry: "2023-06-30" },
-  { week: "Jan 15-21", level: 120, expiry: "2023-06-30" },
+const chickenInventoryData = [
+  { week: "Jan 1-7", level: 40, expiry: "March 16th" },
+  { week: "Jan 8-14", level: 55, expiry: "March 16th" },
+  { week: "Jan 15-21", level: 25, expiry: "March 16th" },
   // Add more data as needed
+  { week: "Jan 22-28", level: 25, expiry: "March 16th" },
+  { week: "Jan 29-Feb 4", level: 45, expiry: "March 16th" },
+  { week: "Feb 5-11", level: 45, expiry: "March 16th" },
+  { week: "Feb 12-18", level: 30, expiry: "March 16th" },
+  { week: "Feb 19-25", level: 30, expiry: "March 16th" },
+  { week: "Feb 26-Mar 4", level: 35, expiry: "March 16th" },
+  { week: "Mar 5-11", level: 35, expiry: "March 16th" },
+  { week: "Mar 12-18", level: 30, expiry: "March 16th" },
 ];
 
-const tomatoesSalesData = [
-  { week: "Jan 1-7", sales: 50 },
-  { week: "Jan 8-14", sales: 70 },
-  { week: "Jan 15-21", sales: 90 },
+const chickenSalesData = [
+  { week: "Jan 1-7", sales: 36 },
+  { week: "Jan 8-14", sales: 49 },
+  { week: "Jan 15-21", sales: 22 },
   // Add more data as needed
+  { week: "Jan 22-28", sales: 23 },
+  { week: "Jan 29-Feb 4", sales: 39 },
+  { week: "Feb 5-11", sales: 41 },
+  { week: "Feb 12-18", sales: 24 },
+  { week: "Feb 19-25", sales: 28 },
+  { week: "Feb 26-Mar 4", sales: 31 },
+  { week: "Mar 5-11", sales: 34 },
+  { week: "Mar 12-18", sales: 28 },
 ];
 
 const lettuceInventoryData = [
-  { week: "Jan 1-7", level: 80, expiry: "2023-07-05" },
-  { week: "Jan 8-14", level: 60, expiry: "2023-07-05" },
-  { week: "Jan 15-21", level: 100, expiry: "2023-07-05" },
+  { week: "Jan 1-7", level: 40, expiry: "March 16th" },
+  { week: "Jan 8-14", level: 45, expiry: "March 16th" },
+  { week: "Jan 15-21", level: 35, expiry: "March 16th" },
   // Add more data as needed
+  { week: "Jan 22-28", level: 35, expiry: "March 16th" },
+  { week: "Jan 29-Feb 4", level: 35, expiry: "March 16th" },
+  { week: "Feb 5-11", level: 40, expiry: "March 16th" },
+  { week: "Feb 12-18", level: 35, expiry: "March 16th" },
+  { week: "Feb 19-25", level: 40, expiry: "March 16th" },
+  { week: "Feb 26-Mar 4", level: 40, expiry: "March 16th" },
+  { week: "Mar 5-11", level: 20, expiry: "March 16th" },
+  { week: "Mar 12-18", level: 35, expiry: "March 16th" },
 ];
 
 const lettuceSalesData = [
-  { week: "Jan 1-7", sales: 30 },
-  { week: "Jan 8-14", sales: 50 },
-  { week: "Jan 15-21", sales: 70 },
+  { week: "Jan 1-7", sales: 37 },
+  { week: "Jan 8-14", sales: 42 },
+  { week: "Jan 15-21", sales: 31 },
   // Add more data as needed
+  { week: "Jan 22-28", sales: 30 },
+  { week: "Jan 29-Feb 4", sales: 29 },
+  { week: "Feb 5-11", sales: 38 },
+  { week: "Feb 12-18", sales: 27 },
+  { week: "Feb 19-25", sales: 33 },
+  { week: "Feb 26-Mar 4", sales: 33 },
+  { week: "Mar 5-11", sales: 13 },
+  { week: "Mar 12-18", sales: 33 },
 ];
 
-let currentTab = "tomatoes"; // Track the current active tab
+let currentTab = "chicken"; // Track the current active tab
 let chart; // Variable to store the chart instance
 
+// Function to render the line graph based on the selected tab
 // Function to render the line graph based on the selected tab
 function renderDashboard() {
   const ctx = document.getElementById("dashboard-chart").getContext("2d");
   let inventoryData, salesData;
 
   // Select the data based on the current tab
-  if (currentTab === "tomatoes") {
-    inventoryData = tomatoesInventoryData;
-    salesData = tomatoesSalesData;
-    document.getElementById("stock-title").textContent = "Tomatoes Information";
+  if (currentTab === "chicken") {
+    inventoryData = chickenInventoryData;
+    salesData = chickenSalesData;
+    document.getElementById("stock-title").textContent = "Chicken Information";
   } else if (currentTab === "lettuce") {
     inventoryData = lettuceInventoryData;
     salesData = lettuceSalesData;
@@ -84,17 +116,20 @@ function renderDashboard() {
     },
   });
 
-  // Update the stock information card with the current stock remaining and last order date
+  // Update the stock information card
   const stockRemaining = inventoryLevels[inventoryLevels.length - 1];
   const lastOrderDate = labels[labels.length - 1];
   const expiryDate = expiryDates[expiryDates.length - 1];
 
-  document.getElementById("stock-remaining").textContent =
-    "Stock Remaining: " + stockRemaining;
-  document.getElementById("last-order-date").textContent =
-    "Last Order Date: " + lastOrderDate;
-  document.getElementById("expiry-date").textContent =
-    "Expiry Date: " + expiryDate;
+  document.getElementById(
+    "stock-remaining"
+  ).textContent = `Stock Remaining: ${stockRemaining}`;
+  document.getElementById(
+    "last-order-date"
+  ).textContent = `Last Order Date: ${lastOrderDate}`;
+  document.getElementById(
+    "expiry-date"
+  ).textContent = `Expiry Date: ${expiryDate}`;
 }
 
 // Function to change the active tab
